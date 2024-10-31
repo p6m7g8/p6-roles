cb17fbd617424b18fab990eb33aded0d2efcc74b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-10-31 14:32:18 -0400

    fix: this works

--- test/p6-roles.test.ts
+++ test/p6-roles.test.ts
@@ -1,4 +1,4 @@
-import type { IP6CDKWebsiteProps } from 'p6-cdk-website-plus'
+import type { IP6CDKGithubOidcProviderProps } from 'p6-cdk-github-oidc-provider'
 import * as process from 'node:process'
 import * as cdk from 'aws-cdk-lib'
 import { Template } from 'aws-cdk-lib/assertions'
@@ -7,20 +7,18 @@ import { MyStack } from '../src/main'
 describe('myStack Snapshot Test', () => {
   it('should match the synthesized CloudFormation template snapshot', () => {
     // the AwsEnvironment
-    const theEnv = {
+    const env = {
       account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
       region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION,
     }
     const app = new cdk.App()
 
-    const site: IP6CDKWebsiteProps = {
-      hostedZoneName: 'p6m7g8.org',
-      verifyEmail: 'pgollucci.com',
-      cloudfrontRecordName: 'www.p6m7g8.org',
+    const site: IP6CDKGithubOidcProviderProps = {
+      repo: 'p6m7g8.org',
     }
 
     // Create the stack
-    const stack = new MyStack(app, 'p6-sites', { site, env: theEnv })
+    const stack = new MyStack(app, 'p6-sites', { ...site, env })
 
     // Prepare the stack template for assertions
     const template = Template.fromStack(stack)
