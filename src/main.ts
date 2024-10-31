ee5100d0fc63142df0aee42d5639fd90475c01f5
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-10-31 19:13:44 -0400

    feat: makes the roles too

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,61 @@
 import type { Construct } from 'constructs'
-import type { IP6CDKGithubOidcProviderProps } from 'p6-cdk-github-oidc-provider'
 import * as process from 'node:process'
 import * as cdk from 'aws-cdk-lib'
 import * as iam from 'aws-cdk-lib/aws-iam'
+import { P6CDKGHARole } from 'p6-cdk-gha-role'
 import { P6CDKGithubOidcProvider } from 'p6-cdk-github-oidc-provider'
 
 export class MyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props)
 
-    const githubOidcProvider: IP6CDKGithubOidcProviderProps = {
+    const githubOidcProvider = new P6CDKGithubOidcProvider(this, 'P6CDKGithubOidcProvider')
+    new P6CDKGHARole(this, 'p6-roles-gha-p6m7g8-p6-roles-role', {
+      principle: githubOidcProvider.openIdConnectProviderArn,
+      repo: 'p6m7g8/p6-roles',
+      policies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')],
+    })
+
+    new P6CDKGHARole(this, 'p6-roles-gha-p6m7g8-p6-cdk-website-plus-role', {
+      principle: githubOidcProvider.openIdConnectProviderArn,
+      repo: 'p6m7g8/p6-cdk-website-plus',
+      policies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')],
+    })
+
+    new P6CDKGHARole(this, 'p6-roles-gha-p6m7g8-p6-sites-role', {
+      principle: githubOidcProvider.openIdConnectProviderArn,
+      repo: 'p6m7g8/p6-sites',
+      policies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')],
+    })
+
+    new P6CDKGHARole(this, 'p6-roles-gha-p6m7g8-p6-domain-records-role', {
+      principle: githubOidcProvider.openIdConnectProviderArn,
+      repo: 'p6m7g8/p6-domain-records',
+      policies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')],
+    })
+
+    new P6CDKGHARole(this, 'p6-roles-gha-p6m7g8-p6-domains-role', {
+      principle: githubOidcProvider.openIdConnectProviderArn,
+      repo: 'p6m7g8/p6-domains',
+      policies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')],
+    })
+
+    new P6CDKGHARole(this, 'p6-roles-gha-p6m7g8-p6m7g8-com-role', {
+      principle: githubOidcProvider.openIdConnectProviderArn,
       repo: 'p6m7g8/p6m7g8.com',
       policies: [
         iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'),
         iam.ManagedPolicy.fromAwsManagedPolicyName('CloudFrontFullAccess'),
       ],
-    }
+    })
 
-    new P6CDKGithubOidcProvider(this, 'P6CDKGithubOidcProvider', {
-      ...githubOidcProvider,
+    new P6CDKGHARole(this, 'p6-roles-gha-pgollucci-gollucci-com-role', {
+      principle: githubOidcProvider.openIdConnectProviderArn,
+      repo: 'pgollucci/gollucci.com',
+      policies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('CloudFrontFullAccess'),
+      ],
     })
   }
 }
