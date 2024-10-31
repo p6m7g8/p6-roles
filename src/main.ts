e25652daa9560585b96b48923ccc0dc4bf7a3b07
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-10-31 13:50:10 -0500

    fix: this works (#3)

--- src/main.ts
+++ src/main.ts
@@ -1,68 +1,30 @@
 import type { Construct } from 'constructs'
-import type { IP6CDKWebsiteProps } from 'p6-cdk-website-plus'
-import * as fs from 'node:fs'
+import type { IP6CDKGithubOidcProviderProps } from 'p6-cdk-github-oidc-provider'
 import * as process from 'node:process'
 import * as cdk from 'aws-cdk-lib'
-import * as yaml from 'js-yaml'
-import { P6CDKWebsitePlus } from 'p6-cdk-website-plus'
+import { P6CDKGithubOidcProvider } from 'p6-cdk-github-oidc-provider'
 
-// Configuration file
-const CONFIG_FILE = 'conf/sites.yml'
-
-/**
- *
- * @param filePath
- * @returns yamlData
- */
-function parseYamlFile(filePath: string): IP6CDKWebsiteProps[] {
-  const fileContents = fs.readFileSync(filePath, 'utf8')
-  const yamlData = yaml.load(fileContents) as IP6CDKWebsiteProps[]
-  return yamlData
-}
-
-/**
- * Interface for the stack
- * @interface ISite
- * @extends {cdk.StackProps}
- * @property {IP6CDKWebsiteProps} site - The site configuration
- * @property {string} site.hostedZoneName - The hosted zone name
- * @property {string} site.verifyEmail - The email to verify
- * @property {string} site.cloudfrontRecordName - The cloudfront record name
- * @property {cdk.StackProps} env - The environment
- */
-interface ISite extends cdk.StackProps {
-  site: IP6CDKWebsiteProps
-}
-/**
- * MyStack
- *
- */
 export class MyStack extends cdk.Stack {
-  /**
-   *
-   * @param scope
-   * @param id
-   * @param props
-   */
-  constructor(scope: Construct, id: string, props: ISite) {
+  constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props)
 
-    new P6CDKWebsitePlus(this, props.site.hostedZoneName, props.site)
+    const githubOidcProvider: IP6CDKGithubOidcProviderProps = {
+      repo: 'p6m7g8/p6m7g8.com',
+    }
+
+    new P6CDKGithubOidcProvider(this, 'P6CDKGithubOidcProvider', {
+      ...githubOidcProvider,
+    })
   }
 }
 
 // the AwsEnvironment
-const theEnv = {
+const env = {
   account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION,
 }
 
 // create the app and stack
 const app = new cdk.App()
-const domains: IP6CDKWebsiteProps[] = parseYamlFile(CONFIG_FILE)
-domains.forEach((domain) => {
-  const stackName = `p6-site-${domain.hostedZoneName.replace(/\./g, '-')}`
-
-  new MyStack(app, stackName, { site: domain, env: theEnv })
-})
+new MyStack(app, 'p6-roles-github-oidc-provider', { env })
 app.synth()
